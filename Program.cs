a668a5f621904440baac7062972633a110bd0831
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-11-08 18:14:06 -0300

    add dictionary AsEnumerable

--- Program.cs
+++ Program.cs
@@ -6,18 +6,17 @@ class Consumer
     static void Main(string[] args)
     {
         Console.WriteLine("Initializing...");
-        var configuration = new Dictionary<string, string>
-        {
-            {"bootstrap-server", "instance-kafka-bootstrap-kafka.apps.ocp.desenv.com:443"},
-            {"security.protocol", "SASL_SSL"},
-            {"ssl.truststore.location", "./ca.p12"},
-            {"ssl.truststore.password", "fX95Ovo90ZEW"},
-            {"ssl.enabled.protocols", "TLSv1.2,TLSv1.1,TLSv1"},
-            {"sasl.mechanism", "SCRAM-SHA-512"},
-            {"sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"sofintech-kafka\" password=\"F3Si8w05cCP6k8AQNtO9W67rDI2Te6uG\";"},
-            {"group.id", "teste-nao-produtivo-group"},
-            {"auto.offset.reset", "earliest"}
-        };
+        
+        var configuration = new Dictionary<string, string>();
+        configuration.Add("bootstrap-server", "instance-kafka-bootstrap-kafka.apps.ocp.desenv.com:443");
+        configuration.Add("security.protocol", "SASL_SSL");
+        configuration.Add("ssl.truststore.location", "./ca.p12");
+        configuration.Add("ssl.truststore.password", "fX95Ovo90ZEW");
+        configuration.Add("ssl.enabled.protocols", "TLSv1.2,TLSv1.1,TLSv1");
+        configuration.Add("sasl.mechanism", "SCRAM-SHA-512");
+        configuration.Add("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"sofintech-kafka\" password=\"F3Si8w05cCP6k8AQNtO9W67rDI2Te6uG\";");
+        configuration.Add("group.id", "teste-nao-produtivo-group");
+        configuration.Add("auto.offset.reset", "earliest");
 
         CancellationTokenSource cts = new CancellationTokenSource();
         Console.CancelKeyPress += (_, e) =>
@@ -26,7 +25,7 @@ class Consumer
             cts.Cancel();
         };
 
-        using (var consumer = new ConsumerBuilder<string, string>(configuration).Build())
+        using (var consumer = new ConsumerBuilder<string, string>(configuration.AsEnumerable()).Build())
         {
             consumer.Subscribe("teste-nao-produtivo");
             try
