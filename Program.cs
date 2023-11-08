15983070982ee8143ffc9e388c8205d757c99b2c
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-11-08 18:56:07 -0300

    add X509Certificate2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Confluent.Kafka;
 using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography.X509Certificates;
 
 class Consumer
 {
@@ -9,16 +10,19 @@ class Consumer
         
         var configuration = new Dictionary<string, string>();
         configuration.Add("bootstrap.servers", "instance-kafka-bootstrap-kafka.apps.ocp.desenv.com:443");
+        configuration.Add("group.id", "teste-nao-produtivo-group");
+        configuration.Add("auto.offset.reset", "earliest");
         configuration.Add("security.protocol", "SASL_SSL");
-        configuration.Add("ssl.ca.location", "/opt/app-root/src/ca.p12");
-        // configuration.Add("ssl.truststore.password", "fX95Ovo90ZEW");
+        
+        X509Certificate2 cert = X509Certificate2("/opt/app-root/src/ca.p12", "fX95Ovo90ZEW");
+        configuration.Add("ssl.ca.pem", cert.ExportCertificatePem());
+        
+        //configuration.Add("ssl.truststore.password", "fX95Ovo90ZEW");
         //configuration.Add("ssl.enabled.protocols", "TLSv1.2,TLSv1.1,TLSv1");
         configuration.Add("sasl.mechanism", "SCRAM-SHA-512");
         configuration.Add("sasl.username", "sofintech-kafka");
         configuration.Add("sasl.password", "F3Si8w05cCP6k8AQNtO9W67rDI2Te6uG");
         //configuration.Add("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"sofintech-kafka\" password=\"F3Si8w05cCP6k8AQNtO9W67rDI2Te6uG\";");
-        configuration.Add("group.id", "teste-nao-produtivo-group");
-        configuration.Add("auto.offset.reset", "earliest");
 
         CancellationTokenSource cts = new CancellationTokenSource();
         Console.CancelKeyPress += (_, e) =>
