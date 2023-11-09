fdb523ce141fdc1a9cefb26f99390f47096097e1
Luis Enrique Gomes Portugal <luis.portugal@sofintech.com.br>
2023-11-09 12:27:19 -0300

    add ConsumerConfig

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-﻿using Confluent.Kafka;
+﻿using System.Security.Authentication;
+using Confluent.Kafka;
 using Microsoft.Extensions.Configuration;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
 
 class Consumer
 {
@@ -9,18 +8,29 @@ class Consumer
     {
         Console.WriteLine("Initializing...");
         
-        var configuration = new Dictionary<string, string>();
-        configuration.Add("bootstrap.servers", "instance-kafka-bootstrap-kafka.apps.ocp.desenv.com:443");
-        configuration.Add("auto.offset.reset", "earliest");
-        configuration.Add("security.protocol", "SASL_SSL");
-        configuration.Add("group.id", "teste-nao-produtivo-group");
+        // var configuration = new Dictionary<string, string>();
+        // configuration.Add("bootstrap.servers", "instance-kafka-bootstrap-kafka.apps.ocp.desenv.com:443");
+        // configuration.Add("auto.offset.reset", "earliest");
+        // configuration.Add("security.protocol", "SASL_SSL");
+        // configuration.Add("group.id", "teste-nao-produtivo-group");
         
-        configuration.Add("ssl.key.location", "/opt/app-root/src/ca.crt");
-        configuration.Add("ssl.key.password", "HQL8lcZ18o4x");
+        // configuration.Add("ssl.key.location", "/opt/app-root/src/ca.crt");
+        // configuration.Add("ssl.key.password", "HQL8lcZ18o4x");
         
-        configuration.Add("sasl.mechanism", "SCRAM-SHA-512");
-        configuration.Add("sasl.username", "sofintech-kafka");
-        configuration.Add("sasl.password", "F3Si8w05cCP6k8AQNtO9W67rDI2Te6uG");
+        // configuration.Add("sasl.mechanism", "SCRAM-SHA-512");
+        // configuration.Add("sasl.username", "sofintech-kafka");
+        // configuration.Add("sasl.password", "F3Si8w05cCP6k8AQNtO9W67rDI2Te6uG");
+
+        var configuration = new ConsumerConfig {
+            AutoOffsetReset = AutoOffsetReset.Earliest,    
+            BootstrapServers = "instance-kafka-bootstrap-kafka.apps.ocp.desenv.com:443",
+            GroupId = "teste-nao-produtivo-group",
+            SecurityProtocol = SecurityProtocol.SaslSsl,
+            SslCertificateLocation = "/opt/app-root/src/ca.crt",  
+            SaslMechanism = SaslMechanism.ScramSha512,
+            SaslUsername = "sofintech-kafka",
+            SaslPassword = "F3Si8w05cCP6k8AQNtO9W67rDI2Te6uG",      
+        };
 
         CancellationTokenSource cts = new CancellationTokenSource();
         Console.CancelKeyPress += (_, e) =>
@@ -29,7 +39,7 @@ class Consumer
             cts.Cancel();
         };
 
-        using (var consumer = new ConsumerBuilder<string, string>(configuration.AsEnumerable()).Build())
+        using (var consumer = new ConsumerBuilder<string, string>(configuration).Build())
         {
             consumer.Subscribe("teste-nao-produtivo");
             try
