0ef81419132227c97c8bf673736918f2db2fde10
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2026-05-29 13:08:08 -0300

    webapi

---
+++ Controllers/ProxyController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProxyController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+
+    public ProxyController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var response = await _httpClient.GetAsync(
+            "https://jsonplaceholder.typicode.com/todos/1"
+        );
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        return StatusCode((int)response.StatusCode, new
+        {
+            status = (int)response.StatusCode,
+            success = response.IsSuccessStatusCode,
+            headers = response.Headers.ToDictionary(
+                h => h.Key,
+                h => string.Join(",", h.Value)
+            ),
+            data = content
+        });
+    }
+}
